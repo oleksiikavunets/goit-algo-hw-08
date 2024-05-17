c9bafe422145330f5a69c032b885af9dc10c5199
Oleksii Kavunets <oleksii.k@appsflyer.com>
2024-05-17 12:56:32 +0200

    upd task

--- cables_joins.py
+++ cables_joins.py
@@ -1,21 +1,33 @@
 import heapq
 
 
+def heap_sort(iterable, descending=False):
+    sign = -1 if descending else 1
+    h = [sign * el for el in iterable]
+    heapq.heapify(h)
+    return [sign * heapq.heappop(h) for _ in range(len(h))]
+
+
 def join_cables(cables):
-    _cables_heap = []
+    def _join(_cables_heap):
+        join_order = []
+        total_sum = 0
+        while len(_cables_heap) != 1:
+            joined_cables = [heapq.heappop(_cables_heap) for _ in range(2)]
 
-    [heapq.heappush(_cables_heap, cable) for cable in cables]
+            join_order.append(tuple(joined_cables))
 
-    _cables_join_order = []
+            total_sum += sum(joined_cables)
+            heapq.heappush(_cables_heap, sum(joined_cables))
+        return total_sum, join_order
 
-    while len(_cables_heap) != 1:
-        joined_cables = [heapq.heappop(_cables_heap) for _ in range(2)]
+    _cables_heap_1 = heap_sort(cables)
+    sum_1, _cables_join_order_1 = _join(_cables_heap_1)
 
-        _cables_join_order.append(tuple(joined_cables))
+    _cables_heap_2 = heap_sort(cables, descending=True)
+    sum_2, _cables_join_order_2 = _join(_cables_heap_2)
 
-        heapq.heappush(_cables_heap, sum(joined_cables))
-
-    return _cables_join_order
+    return _cables_join_order_1 if sum_1 < sum_2 else _cables_join_order_2
 
 
 cables = [i for i in range(50, 1, -3)]
